45b3f6f8736e3e64b384c8a1def39a33b5316bcc
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2023-04-01 17:34:26 -0700

    Lesson 6 | Final Task

---
+++ week06/scripts/task6.js
@@ -0,0 +1,46 @@
+// Set up canvas
+const canvas = document.getElementById("spaceCanvas");
+const ctx = canvas.getContext("2d");
+
+// Cube vars
+const square = {
+    sideLength: 10,
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    dy: 0,
+    gravity: 0.5, 
+    dySpace: -10
+};
+
+// game loop
+function gameLoop() {
+    document.body.onkeydown = function(event) {
+        if (event.code == "Space") {
+            square.dy = square.dySpace;
+        }
+    }
+
+    square.y += square.dy;
+    square.dy += square.gravity;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(square.x, square.y, square.sideLength, square.sideLength);
+
+    keepSquareInCanvas();
+}
+
+// Make sure square stays in canvas and reset accel if collision
+function keepSquareInCanvas () {
+    if (square.y > canvas.height - square.sideLength) {
+        square.y = canvas.height - square.sideLength;
+        square.dy = 0;
+    }
+    else if (square.y < square.sideLength) {
+        square.y = square.sideLength;
+        square.dy *= -.5;
+    }
+}
+
+// Game init
+setInterval(gameLoop, 10);
